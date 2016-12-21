1933936a3d14c05271ffcc566e34746fcb269258
youxianQ <qin1040392240@hotmail.com>
2016-12-21 13:46:51 +0800

    remove unused html/js

--- static/js/controllers/signin.js
+++ static/js/controllers/signin.js
@@ -10,7 +10,7 @@ app.controller('SigninFormController', ['$scope', '$http', '$state','authService
       // Try to login
       if (authService.returnUser()!== undefined) {
         $scope.authError = "重复登陆";
-        $state.go('app.dashboard-v1');
+        $state.go('docker.dashboard');
       }
       $http.post('api/auth/sign', {Username: $scope.user.name, Password: $scope.user.password})
           .then(function(response) {
@@ -18,11 +18,11 @@ app.controller('SigninFormController', ['$scope', '$http', '$state','authService
             $scope.authError = response.data.info;
             if (response.data.data != null && "Username" in response.data.data) {
               authService.login(response.data.data.Username);
-              $state.go('app.dashboard-v1');
+              $state.go('docker.dashboard');
             }
           }else{
           authService.login(response.data.data.Username);
-          $state.go('app.dashboard-v1');
+          $state.go('docker.dashboard');
         }
       }, function(x) {
         $scope.authError = 'Server Error';
